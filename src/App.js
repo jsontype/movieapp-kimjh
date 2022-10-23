4fb97f23cb0f2118e537c47f0c1704072a339421
azumaapp <ydhvocal@gmail.com>
2022-10-23 19:07:25 +0900

    feat: add api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import MovieList from './components/MovieList'
 
 function App() {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
+      .then((res) => { return res.json() })
+      .then((json) => { setMovies(json.data.movies) })
+  }, [])
+
+  console.log('movies: ', movies)
+
   return (
     <div className="App">
-      Hello
+      <h1>영화 앱</h1>
+      <MovieList movies={movies} />
     </div>
   )
 }
