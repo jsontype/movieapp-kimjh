e8a7983b48d3d844d3a40eddb7931cb7f016eeca
mackjh <gumika@Gumikaui-MacBook-Pro.local>
2022-10-23 21:18:41 +0900

    feat: add display genres

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -6,10 +6,16 @@ export default function MovieList({ movies }) {
   const render = movies.map((item) => {
     const ratingClass = item.rating >= 8 ? "ratingGood" : item.rating >= 6 ? "ratingSoso" : "ratingBad"
     const ratingIcon = item.rating >= 8 && "🎇"
+    const genres = item.genres.join(', ')
 
     return (
       <div className='movie' key={item.id}>
-        <a className='movieTitle' href={item.url}>{item.title} {ratingIcon} &#40; 평점 : <span className={ratingClass}>{item.rating}</span> / 10 &#41;</a>
+        <a className='movieTitle' href={item.url}>
+          {item.title} {ratingIcon} 
+          &#40; 평점 : <span className={ratingClass}>{item.rating}</span> / 10 &#41;
+          <div>{genres}</div>
+        </a>
+        
         <div className='movieYear'>{item.year}</div>
         <img className='movieImage' src={item.large_cover_image} alt={item.title}></img>
       </div>
