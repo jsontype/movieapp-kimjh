d3babb8dd3d375d1125127899640d8ce56ae559f
mackjh <gumika@Gumikaui-MacBook-Pro.local>
2022-10-23 20:37:14 +0900

    feat: edited rating color

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
   const [movies, setMovies] = useState([])
 
   useEffect(() => {
-    fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
+    fetch('https://yts.mx/api/v2/list_movies.json')
       .then((res) => { return res.json() })
       .then((json) => { setMovies(json.data.movies) })
   }, [])
 
-  // console.log('movies: ', movies)
+  console.log('movies: ', movies)
 
   return (
     <div className="App">
--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -4,12 +4,11 @@ import './style.css'
 export default function MovieList({ movies }) {
   
   const render = movies.map((item) => {
-    const ratingClass = item.rating >= 9 ? "ratingGood" : item.rating >= 7 ? "ratingSoso" : "ratingBad"
-    const ratingIcon = item.rating >= 9 && "🔥"
+    const ratingClass = item.rating >= 8 ? "ratingGood" : item.rating >= 6 ? "ratingSoso" : "ratingBad"
 
     return (
       <div className='movie' key={item.id}>
-        <a className='movieTitle' href={item.url}>{item.title} {ratingIcon} &#40; 평점 : <span className={ratingClass}>{item.rating}</span> / 10 &#41;</a>
+        <a className='movieTitle' href={item.url}>{item.title} &#40; 평점 : <span className={ratingClass}>{item.rating}</span> / 10 &#41;</a>
         <div className='movieYear'>{item.year}</div>
         <img className='movieImage' src={item.large_cover_image} alt={item.title}></img>
       </div>
@@ -18,7 +17,7 @@ export default function MovieList({ movies }) {
 
   return (
     <>
-      {render}
+          {render}
     </>
   )
 }
