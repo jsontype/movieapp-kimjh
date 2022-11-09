6a5f358dc5d164b2f502973db49fb837d3e38eec
kjh-busan <picsmash7@gmail.com>
2022-11-09 13:22:51 +0900

    feat:30 update for scss

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import AppTitle from './components/AppTitle'
 import MovieList from './components/MovieList'
 import NewsList from './components/NewsList'
 
-import './App.css'
+import './App.scss'
 
 function App() {
   // JS
--- src/components/MovieList/MovieDetail/index.tsx
+++ src/components/MovieList/MovieDetail/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './style.scss'
 
 type MovieDetailProps = {
     item: any
