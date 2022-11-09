dc8d4d61433db9090de0327b1ae7b90fe303222d
jsontype <jsontyper@gmail.com>
2022-11-09 19:11:44 +0900

    fix: error

--- src/components/MovieList/MovieDetail/index.tsx
+++ src/components/MovieList/MovieDetail/index.tsx
@@ -11,27 +11,30 @@ export default function MovieDetail({ item }: MovieDetailProps) {
     const genres = item.genres.join(', ')
     const runtime = item.runtime + "min"|| "상영시간 정보없음"
     const summary = item.summary ? item.summary : item.synopsis
-    const copyUrl = (url: string, idx: number) => {
-        navigator.clipboard.writeText(url)
-        console.log("토렌토" + idx + " 주소가 복사되었습니다")
-    }
+    // const copyUrl = (url: string, idx: number) => {
+    //     navigator.clipboard.writeText(url)
+    //     console.log("토렌토" + idx + " 주소가 복사되었습니다")
+    // }
 
     const torrents = item.torrents.map((item: any, idx: number) => {
         return (
-            <div key={idx}>토렌트{idx + 1}&nbsp;
-                <button type="button" onClick={ () => copyUrl(item.url, idx + 1)}>주소복사</button>
-                &nbsp;{item.url}
-            </div>
+            // <div key={idx}>토렌트{idx + 1}&nbsp;
+            //     <button type="button" onClick={() => copyUrl(item.url, idx + 1)}>주소복사</button>
+            //     &nbsp;{item.url}
+            // </div>
+            <span key={idx}>
+                <a href={item.url}>토렌트{idx + 1}</a>&nbsp;
+            </span>
         )
     })
     
     const detail = (
         <div className={styles.movieDetail}>
+            <div>평점 : <span className={styles[movieRank]}>{item.rating}</span> / 10 <span>{hotIcon}</span></div>
             <div>장르: {genres}</div>
             <div>러닝타임: {runtime}</div>
-            <div><span>{hotIcon}</span> 평점 : <span className={movieRank}>{item.rating}</span> / 10</div>
-            <div>{summary}</div>
-            <div>{torrents}</div>
+            <div>줄거리 : {summary}</div>
+            <div>토렌트 : {torrents}</div>
         </div>
     )
 
